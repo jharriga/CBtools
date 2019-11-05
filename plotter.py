038915ec272123f81fac1fca8b9985810a864607
John Harrigan <jharriga@redhat.com>
2019-11-05 08:44:19 -0600

    rewritten to add AVG times in Legend

--- plotter.py
+++ plotter.py
@@ -27,9 +27,11 @@ def plot_stats(theFile, plotType, key1, key2, yUnits):
     clr = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black']
     ts_created = os.stat(theFile).st_ctime
     dt_created = dt.datetime.fromtimestamp(ts_created)
+    
     print "PROCESSING - plotting Statistics:", plotType
     print "> Reading data from: {}".format(theFile)
     print "> File creation time: ", str(dt_created)
+
     with open(theFile,'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
 # Parse the first row looking for column locations
@@ -43,63 +45,69 @@ def plot_stats(theFile, plotType, key1, key2, yUnits):
         opLabel = []
         for colNum in range(colIndex1, colIndex2, 1):
             opLabel.append(header2[colNum])
+        numLabels=len(opLabel)
         print "> Operation Column range: ", colIndex1, colIndex2
+        print "> Operation Count is: ", numLabels
         print "> Operations found: ", opLabel   # array of operation labels
 # Initialize the opStats 2D array - holds table of statistics
 #   2D array structure is opStats[opLabel#][row#]
-        numLabels=len(opLabel)
-        opStats = [[] for i in range(numLabels)]  
+##        opStats = [[], []]
+        opStats = [[] for _ in range(numLabels)]
+        totValue = []        # calculating average
 # Make sure we have enough COLORS. One for each Operation
         numClrs = len(clr)
         if numLabels > numClrs:
             print "Too many operations %d. MAX of %d . Exiting", numLabels, numClrs
             sys.exit()
 # NOW we are ready to read the actual statistics
-        incr = int(0)           # increments timestamp on 24hr period
-        ocntr = int(0)          # outer loop counter
+        incrDay = int(0)        # increments timestamp on 24hr period
+        rowCntr = int(0)        # outer loop counter - rows
+##        totValue = [0.0, 0.0, 0.0, 0.0]        # calculating average
+# initialize the array which contains averages
+        for i in range (numLabels):
+            totValue.append(float(0.0))
+
 # parse all the remaining rows
         for row in plots:
             cos_ts = dt.datetime.strptime(row[0],"%H:%M:%S")
             this = cos_ts.replace(year=dt_created.year, month=dt_created.month, day=dt_created.day)
-            if (ocntr > 0):                 # check for day rollover 
+            if (rowCntr > 0):               # check for day rollover 
                 if (prev > this.hour):
-                    incr += 1               # add a day
+                    incrDay += 1            # add a day
                     print ">* Adding a day at: ", str(this)
-            date = this + dt.timedelta(days=incr) 
+            date = this + dt.timedelta(days=incrDay) 
             time.append(date)
 
-            icntr = int(0)           # inner loop counter
+            colCntr = int(0)           # inner loop counter - columns
             for column in range(colIndex1, colIndex2, 1):
-                opStats[icntr].append(row[column])
-                icntr += 1
+                value = float(row[column])
+                opStats[colCntr].append(value)
+                totValue[colCntr] = totValue[colCntr] + value
+                colCntr += 1
 
             prev=this.hour
-            ocntr += 1
+            rowCntr += 1
+
     xticks=len(time)                # number of samples
     print "> Number of time values: ", str(xticks)
 
-    fig = plt.figure(figsize=(18,6))
+    fig = plt.figure(figsize=(18,7))
     ax1 = fig.add_subplot(111)
-    ax1.yaxis.grid(True)
-    ax1.xaxis.grid(True)
-    ax1.yaxis.grid(color='#CDCDC1', linestyle='-', linewidth=1)
-    ax1.xaxis.grid(color='#CDCDC1', linestyle='-', linewidth=1)
-    ax1.set_axisbelow(True)
-# Add actual timestamp data on Xaxis
-    xfmt=md.DateFormatter('%H:%M:%S')
-    ax1.xaxis.set_major_formatter(xfmt)
+    plt.xticks(rotation=45)
+
 # Plot the stats for each of the operation labels
     for x in range(0, numLabels, 1):
-        ax1.plot(time, opStats[x], color=clr[x], linewidth=1, marker="o", label=opLabel[x])
- 
-    plt.tick_params(axis='both', which='major', labelsize=18)
-    plt.tick_params(axis='both', which='minor', labelsize=18)
+        thisAvg = round(float(totValue[x] / xticks), 2)
+        labelStr = opLabel[x] + "=" + str(thisAvg)
+##        ax1.plot(time, opStats[x], color=clr[x], linewidth=1, marker="o", label=opLabel[x])
+        ax1.plot(time, opStats[x], color=clr[x], linewidth=1, marker="o", label=labelStr)
+
 # Print the X and Y axis labels
     yType = str(plotType)
     yLabel = str(yType.upper()+yUnits)
     plt.ylabel(str(yLabel), fontsize=20)
     plt.xlabel('TIMESTAMP (%d samples)' % xticks, fontsize=20)
-    # print the legand box (with color-coded operations)
+    # print the legend box (with color-coded operations)
     leg=plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=4, mode="expand", borderaxespad=0., fontsize=19)
     plt.subplots_adjust(left=0.12,bottom=0.18, right=0.9, top=0.85, wspace=0.2,hspace=0.2)
 # use the filename as the plot title
@@ -130,15 +138,15 @@ if __name__ == "__main__":
     if args.type == 'latency':
 # set up for plotting latency/ResponseTimes
         plot = "latency"
-        label1 = "Avg-ResTime"
-        label2 = "Avg-ProcTime"
+        colStart = "Avg-ResTime"
+        colEnd = "Avg-ProcTime"
         units = " (ms)"
     elif args.type == 'throughput':
 # set up for plotting Throughput
         plot = "throughput"
-        label1 = "Throughput"
-        label2 = "Bandwidth"
-        units = " (ops)"
+        colStart = "Throughput"
+        colEnd = "Bandwidth"
+        units = " (op/s)"
     else:
         print "Unsupported plot type requested:", args.type
         print "Supported types are: latency, throughput"
@@ -150,11 +158,11 @@ if __name__ == "__main__":
     for file in list(glob.glob(args.dir+'/s?-*.csv')):
         if (file.find("-worker.csv") == -1) and (file.find("-init_") == -1):
             print "Plotting CSV file: ", file
-            plot_stats(file, plot, label1, label2, units)
+            plot_stats(file, plot, colStart, colEnd, units)
         else:
             print "Skipping CSV file: ", file
 
-# Wait for user input
+# Wait for user input - DEBUG
 #        os.system('read -s -n 1 -p "Press any key to continue..."')
 #        print
 
